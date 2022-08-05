24a6b5c5b2eb37faa9228e315d3f7a0499e9be41
Andrew Lindstrom <lindstrom.32@osu.edu>
2022-08-05 11:05:57 -0700

    updated fetching python script

--- national-park-transit/fetch_park_data.py
+++ national-park-transit/fetch_park_data.py
@@ -1,17 +1,53 @@
 """Fetch data on all National Parks in a given region"""
 
+from typing import Union
 import os
 import geopandas as gpd
-from overpass_tools import run_overpass_query
 
-BASE_PATH = os.path.dirname(__file__)
-QUERY_PATH = os.path.join(BASE_PATH, "queries")
-DATA_PATH = os.path.join(BASE_PATH, "data")
+from overpass_tools import run_overpass_query
+from constants import BASE_PATH, QUERY_PATH, DATA_PATH
+
+def fetch_transit_data(
+    relation_id: int,
+    output_location: os.PathLike = None
+) -> Union[gpd.GeoDataFrame, None]:
+    """Fetch transit data for park"""
+    query_path = os.path.join(
+        QUERY_PATH,
+        "transit_routes_in_parks.op"
+    )
+    if relation_id == 1870066:
+        query_path = os.path.join(
+            QUERY_PATH,
+            "transit_routes_in_parks_1870066.op"
+        )
+    with open(query_path, 'r') as f:
+        query = f.read()
+    query = query.format(relation_id)
+    #print(query)
+    cache_path = os.path.join(
+        DATA_PATH,
+        "transit_routes",
+        f"transit_data_{relation_id}.geojson"
+    )
+    os.makedirs(os.path.dirname(cache_path),exist_ok=True)
+
+    transit_data = run_overpass_query(
+        query=query,
+        cache_path=cache_path,
+        #verbosity="geom"
+    )
+    if not transit_data.empty:
+        return transit_data
+    else:
+        return gpd.GeoDataFrame(
+            columns=["type","id","tags","geometry"]
+        )
 
 def fetch_park_data(
     country_name: str = "United States",
-    output_location: os.PathLike = None
-) -> bool:
+    cache_transit_data: bool = True
+) -> gpd.GeoDataFrame:
     """only US for now"""
     if country_name != "United States":
         raise NotImplementedError("Only support for US National Parks for now")
@@ -24,15 +60,24 @@ def fetch_park_data(
         query = f.read()
     cache_path = os.path.join(
         DATA_PATH,
-        "united_states_national_parks.geojson"
+        f"{country_name}_national_parks.geojson"
     )
     park_data = run_overpass_query(
         query=query,
-        cache_path=cache_path
+        cache_path=cache_path,
+        verbosity="geom"
     )
-
-    # process into a simpler, regular pandas df without geom data too
-    print(park_data)
+    park_data = park_data[park_data["type"]=="relation"]
+    if not cache_transit_data:
+        return park_data
+    # fetch all potentially interesting transit routes and cache
+    for idx in park_data.index:
+        park = park_data.loc[idx]
+        print(f"Fetching transit data for {park['tags'].get('name')}")
+        park_id = park["id"]
+        transit_data = fetch_transit_data(park_id)
+        
+    return park_data
 
 if __name__ == "__main__":
     fetch_park_data()
